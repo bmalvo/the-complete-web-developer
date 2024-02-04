513cf4dc1cb5b8df5e24df7c677a2a8530940347
bmalvo <boydmalvo@gmail.com>
2024-02-04 16:20:26 +0100

    excercise5

---
+++ javascript/script.js
@@ -0,0 +1,22 @@
+function checkDriverAge(age) {
+    
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
+
+let checkDriverAge2 = () => {
+    var age = prompt("What is your age?");
+    
+    if (Number(age) < 18) {
+        alert("Sorry, you are too yound to drive this car. Powering off");
+    } else if (Number(age) > 18) {
+        alert("Powering On. Enjoy the ride!");
+    } else if (Number(age) === 18) {
+        alert("Congratulations on your first year of driving. Enjoy the ride!");
+    }
+}
