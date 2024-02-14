7c5a40cba93728e60f588cff0e812f91591790b5
bmalvo <boydmalvo@gmail.com>
2024-02-14 12:55:46 +0100

    add ex7

---
+++ javascript/ex7.js
@@ -0,0 +1,22 @@
+let users = {
+    usernname: 'Wojtek',
+    password: 'par'
+};
+
+let database = [users];
+
+let newsfeed = [
+    {
+    usernname: 'Wojtek',
+    timeline: 'So tired from all that learning!'
+},
+    {
+    usernname: 'Bob',
+    timeline: 'Javascript is sooooo cool!',
+    },
+    {
+    usernname: 'Julia',
+    timeline: 'Javascript is preeetyy cool!'
+}]
+
+console.log(newsfeed)
