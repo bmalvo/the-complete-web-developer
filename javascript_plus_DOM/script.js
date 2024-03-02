2e8f15962d0b1b102ff43ce3bf53d9d4664336a2
bmalvo <boydmalvo@gmail.com>
2024-03-02 16:29:13 +0100

    add toggle class "done", add delete button for items

--- javascript_plus_DOM/script.js
+++ javascript_plus_DOM/script.js
@@ -1,11 +1,13 @@
 const btn = document.querySelector('#enter');
 const userInput = document.querySelector('#userInput');
 const ul = document.querySelector('ul');
+const items = document.querySelectorAll('li');
 
 const createListElem = () => {
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(userInput.value));
     ul.appendChild(li);
+    li.addEventListener('click', addButton(li));
     userInput.value = '';
 }
 
@@ -23,5 +25,21 @@ const addafterKeydown = (event) => {
 };
 
 btn.addEventListener('click', addAfterClick);
-
 userInput.addEventListener('keydown', addafterKeydown);
+
+const addButton = (el) => { 
+    el.addEventListener('click', () => {
+        el.classList.toggle('done');
+        isButton = el.lastChild.nodeName.toString() === 'BUTTON'
+        if (!isButton) {    
+            const newButton = document.createElement('button');
+            newButton.textContent = 'delete';
+            el.appendChild(newButton);
+            newButton.addEventListener('click', () => {
+                el.remove();
+            })
+        }
+    });
+}
+
+items.forEach(el => addButton(el));
