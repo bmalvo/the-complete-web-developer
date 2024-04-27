21c77e2b03589379dcc377e3bb071fe9e0ca6d6c
bmalvo <boydmalvo@gmail.com>
2024-04-27 12:20:00 +0200

    resolve exerciseES7 task4

--- tasks/exerciseES7.js
+++ tasks/exerciseES7.js
@@ -26,3 +26,6 @@ console.log(powerOf3(2))
 // #4) Useing your function from #3, put in the paramter 10000. What is the result?
 // Research for yourself why you get this result
 
+console.log(powerOf3(10000)); // infinity
+
+// infinity is a special value that represents an infinitely large number. There are two types of infinity in JavaScript: positive infinity and negative infinity. Positive infinity is greater than any finite number, and negative infinity is less than any finite number.
