75f54c41227d771d472362a02b266be8a60a9801
bmalvo <boydmalvo@gmail.com>
2024-04-27 12:11:00 +0200

    resolve exerciseES7 task1

--- tasks/exerciseES7.js
+++ tasks/exerciseES7.js
@@ -4,10 +4,14 @@
 // #1) Check if this array includes the name "John".
 const dragons = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
 
+const ifIncludes = dragons.includes('John') ? 'yes' : 'nope';
+console.log(ifIncludes); 
+
 
 // #2) Check if this array includes any name that has "John" inside of it. If it does, return that
 // name or names in an array.
 const dragons2 = ['Tim', 'Johnathan', 'Sandy', 'Sarah'];
+
 
 
 // #3) Create a function that calulates the power of 100 of a number entered as a parameter
