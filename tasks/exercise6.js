340de5c10334e5db01443e064d423e5932c7557a
bmalvo <boydmalvo@gmail.com>
2024-04-27 11:58:07 +0200

    resolve exercise6 task3

--- tasks/exercise6.js
+++ tasks/exercise6.js
@@ -13,3 +13,24 @@ object1.a = 4;
 
 //#3 create two classes: an Animal class and a Mamal class. 
 // create a cow that accepts a name, type and color and has a sound method that moo's her name, type and color. 
+
+class Animal {
+    constructor(name, type, color) {
+        this.name = name;
+        this.type = type;
+        this.color = color;
+    }
+}
+
+class Mamal extends Animal {
+    constructor(name, type, color) {
+        super(name, type, color);
+    }
+
+    moo() {
+        console.log(`I am a ${this.name}, a ${this.type} ${this.color} cow! Mooo!!!`)
+    }
+}
+
+const cow = new Mamal('Lily', 'Holender', 'red');
+cow.moo()
