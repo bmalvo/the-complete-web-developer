3e4548ffa6942b2a340a288aebe1fa2836105a72
bmalvo <boydmalvo@gmail.com>
2024-04-28 15:17:46 +0200

    resolve ES10-exercise task 6

--- tasks/ES10-Exercise.js
+++ tasks/ES10-Exercise.js
@@ -38,6 +38,9 @@ console.log(fixedUserEmail3); // cannotfillemailformcorrectly@gmail.com
 //#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 const users = { user1: 18273, user2: 92833, user3: 90315 }
 
+const usersArray = Object.entries(users)
+
+console.log(usersArray); // [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
 
 //#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
 
