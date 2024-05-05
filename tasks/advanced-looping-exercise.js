b3db5f5a83763780a0b9aeb6aad6f8d885c5a291
bmalvo <boydmalvo@gmail.com>
2024-05-05 20:43:23 +0200

    resolved advanced-looping-exercise.js

--- tasks/advanced-looping-exercise.js
+++ tasks/advanced-looping-exercise.js
@@ -6,22 +6,20 @@ const detailedBasket = {
 }
 
 //1
-for (let i = 0; i < basket.length; i++) {
-  console.log(basket[i]);
-}
+// for (let i = 0; i < basket.length; i++) {
+//   console.log(basket[i]);
+// }
 
 //2
-basket.forEach(item => {
-  console.log(item);
-})
+// basket.forEach(item => console.log(item));
 
-for (item in detailedBasket) {
-  console.log(item);
-}
+// for (item in detailedBasket) {
+//   console.log(item);
+// }
 
-for (item of basket) {
-  console.log(item);
-}
+// for (item of basket) {
+//   console.log(item);
+// }
 
 // Question #1:
 // create a function called biggestNumberInArray() that takes
@@ -33,17 +31,50 @@ const array2 = ['a', 3, 4, 2] // should return 4
 const array3 = [] // should return 0
 
 function biggestNumberInArray(arr) {
+  let highest = !arr ? -Infinity : 0;
 
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i])
+    if (arr[i] > highest) {
+      highest = arr[i]
+    }
+  }
+
+  return highest
 }
+console.log(biggestNumberInArray(array))
+console.log(biggestNumberInArray(array2))
+console.log(biggestNumberInArray(array3))
 
 function biggestNumberInArray2(arr) {
+  let highest = !arr ? -Infinity : 0;
 
+  for (el of arr) {
+    if (el > highest) {
+      highest = el
+    };
+  };
+
+  return highest
 }
+console.log(biggestNumberInArray2(array))
+console.log(biggestNumberInArray2(array2))
+console.log(biggestNumberInArray2(array3))
 
 function biggestNumberInArray3(arr) {
+  let highest = !arr ? -Infinity : 0;
 
+  arr.forEach(el => {
+    if (el > highest) {
+      highest = el;
+    }
+  });
+
+  return highest
 }
-
+console.log(biggestNumberInArray3(array))
+console.log(biggestNumberInArray3(array2))
+console.log(biggestNumberInArray3(array3))
 
 // Question #2:
 // Write a function checkBasket() that lets you know if the item is in the basket or not
@@ -54,5 +85,16 @@ amazonBasket = {
 }
 
 function checkBasket(basket, lookingFor) {
+  
+  res = `There is no ${lookingFor}.`
 
-}
+  for (element in basket) {
+    if (element == lookingFor) {
+      res = `We have ${lookingFor} in store`
+    }
+  }
+  return res
+};
+
+console.log(checkBasket(amazonBasket, 'books'))
+console.log(checkBasket(amazonBasket, 'gun'))
